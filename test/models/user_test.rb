bb3df5b2fcfd8f9e2107c20ae5c17a48d3066876
landroide13 <lazaro02@hotmail.com>
2017-08-09 16:00:17 +0700

    milestone 3

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,29 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # test "add_friend should add a stranger user as a friend " do
+  #   a = User.create(name:"test1", email:"test@com", password:"1111")
+  #   b = User.create(name:"test2", email:"test2@com", password:"2222")
+
+  #   a.add_friend(b)
+
+  #   assert_includes a.friends, b
+  #   assert_equal 1, a.friends.count
+  # end
+
+
+  test "add_friend should not add a stranger as a friend twice" do
+    a = User.create(name:"test1", email:"test@com", password:"1111")
+    b = User.create(name:"test2", email:"test2@com", password:"2222")
+
+    a.add_friend(b)
+    a.add_friend(b)
+
+    #assert_includes a.friends, b
+    assert_equal(1, a.friends.count)
+
+    assert 5 == 4+1
+  end
+
 end
